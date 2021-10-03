cd7f8e460ee8ecc3ca440175fd4f070766a21e52
izunyan <izugaeru@gmail.com>
2021-10-04 00:32:00 +0900

    nest_tabyl

--- test/memo.R
+++ test/memo.R
@@ -38,3 +38,44 @@ str_view(text, "(....)\\1")
 str_view(text, "(.+)_\\1")
 # str_view(text, "(....)_\\2")
 # str_view(text, "(....)\\1_\\1")
+
+
+
+
+map(vars, ~ count(mtcars, .data[[.x]])) %>% 
+  set_names(vars) 
+
+cross_tabyl = function(df, value_var, nest_vars = NULL){
+  df %>%
+    nest(data = -c({{nest_vars}})) %>%
+    mutate(freq =
+             map(data, ~ tabyl(.x, {{value_var}}))) %>%
+    select(-data) %>%
+    unnest(cols = c(freq)) %>%
+    as_tibble()
+}
+
+# 関数
+## ...について
+## https://adv-r.hadley.nz/functions.html#fun-dot-dot-dot
+## dot-dot-dot
+
+
+# https://stackoverflow.com/questions/49594820/dplyr-mutate-at-and-case-when
+library(psych)
+bfi_tb <- as_tibble(bfi)
+
+bfi %>%
+  mutate_at(vars(A1:A2),
+            funs(低値 = case_when(
+              . <= 4 ~ 1,
+              TRUE ~ 0
+            ))
+  ) %>%
+  select(A1,A2,A1_低値,A2_低値)
+
+df %>%
+  mutate(across(everything(),
+                ~case_when(. == 0~1,
+                           . > 4 ~4,
+                           TRUE ~ .)))
